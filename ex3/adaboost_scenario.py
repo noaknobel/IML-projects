4d502fed47a993217f2b860109cc88558c83f18a
Noa <noaknobel@gmail.com>
2025-05-19 23:24:57 +0300

    finished ada

--- ex3/adaboost_scenario.py
+++ ex3/adaboost_scenario.py
@@ -73,63 +73,51 @@ def fit_and_evaluate_adaboost(noise, n_learners=250, train_size=5000, test_size=
             pickle.dump((train_X, train_y, test_X, test_y, ab_model), f)
 
     # Question 1: Train- and test errors of AdaBoost in noiseless case
-    # question_1(ab_model, n_learners, test_X, test_y, train_X, train_y)
-
-    # Question 2: Plotting decision surfaces
-    question_2(ab_model, test_X, test_y, train_X)
-
-    # # Question 3: Decision surface of best performing ensemble
-    # raise NotImplementedError()
-    #
-    # # Question 4: Decision surface with weighted samples
-    # raise NotImplementedError()
-
-
-def question_2(ab_model, test_X, test_y, train_X):
-    T = [5, 50, 100, 250]
-    lims = np.array([np.r_[train_X, test_X].min(axis=0), np.r_[train_X, test_X].max(axis=0)]).T + np.array([-.1, .1])
-    fig = make_subplots(rows=2, cols=2,
-                        subplot_titles=[f"T = {t}" for t in T],
-                        horizontal_spacing=0.05, vertical_spacing=0.07)
-    for i, t in enumerate(T):
-        r, c = divmod(i, 2)
-        row, col = r + 1, c + 1
-
-        fig.add_traces([
-            decision_surface(
-                lambda X, T=t: ab_model.partial_predict(X, T),
-                lims[0], lims[1],
-                showscale=False
-            ),
-            go.Scatter(
-                x=test_X[:, 0], y=test_X[:, 1],
-                mode="markers", showlegend=False,
-                marker=dict(
-                    color=test_y,
-                    symbol=[class_symbols[int((y_i + 1) // 2)] for y_i in test_y],
-                    colorscale=px.colors.diverging.RdBu,
-                    line=dict(color="black", width=1),
-                    size=6
-                )
-            )
-        ], rows=row, cols=col)
-
-        # set axis ranges
-        fig.update_xaxes(range=lims[0], row=row, col=col)
-        fig.update_yaxes(range=lims[1], row=row, col=col)
-    fig.update_layout(
-        title_text="AdaBoost Partial Decision Surfaces (noise=0)",
-        height=800, width=800,
-    )
-    fig.write_html("q2_adaboost_surfaces.html", include_plotlyjs="cdn", full_html=True)
-
-
-def question_1(ab_model, n_learners, test_X, test_y, train_X, train_y):
     # Compute training & test errors for t = 1…250
     ts = np.arange(1, n_learners + 1)
     train_err = [ab_model.partial_loss(train_X, train_y, t) for t in ts]
     test_err = [ab_model.partial_loss(test_X, test_y, t) for t in ts]
-    # Plot both curves
+    # plot_test_train_err(test_err, train_err, ts)
+
+    # Question 2: Plotting decision surfaces
+    lims = np.array([np.r_[train_X, test_X].min(axis=0), np.r_[train_X, test_X].max(axis=0)]).T + np.array([-.1, .1])
+    # comparing_ensemble_sizes(ab_model, test_X, test_y, lims)
+
+    # Question 3: Decision surface of best performing ensemble
+    best_t = int(np.argmin(test_err)) + 1
+    best_acc = 1 - test_err[best_t - 1]
+    # plot_best_ensemble_size(ab_model, best_acc, best_t, lims, test_X, test_y)
+
+    # Question 4: Decision surface with weighted samples
+    plot_weighted_train(ab_model, lims, train_X, train_y)
+
+
+def plot_weighted_train(ab_model, lims, train_X, train_y):
+    D_final = ab_model.D_[-1]
+    sizes = D_final / np.max(D_final) * 5
+    fig4 = go.Figure([decision_surface(lambda X: ab_model.predict(X), lims[0], lims[1], showscale=False),
+                      go.Scatter(x=train_X[:, 0], y=train_X[:, 1], mode="markers", showlegend=False,
+                                 marker=dict(size=sizes, color=train_y,
+                                             symbol=[class_symbols[int((y_i + 1) // 2)] for y_i in train_y],
+                                             colorscale=px.colors.diverging.RdBu, line=dict(color="black", width=1),
+                                             sizemode="area"))])
+    fig4.update_layout(title="Decision Surface with Training‐Point Weights", xaxis=dict(range=lims[0], visible=False),
+                       yaxis=dict(range=lims[1], visible=False), width=600, height=600)
+    fig4.write_html("q4_weighted_train_surface.html", include_plotlyjs="cdn", full_html=True)
+
+
+def plot_best_ensemble_size(ab_model, best_acc, best_t, lims, test_X, test_y):
+    fig = go.Figure([
+        decision_surface(lambda X, T=best_t: ab_model.partial_predict(X, T), lims[0], lims[1], showscale=False),
+        go.Scatter(x=test_X[:, 0], y=test_X[:, 1], mode="markers", showlegend=False,
+                   marker=dict(color=test_y, symbol=[class_symbols[int((y_i + 1) // 2)] for y_i in test_y],
+                               colorscale=px.colors.diverging.RdBu, line=dict(color="black", width=1), size=6))
+    ], layout=go.Layout(width=500, height=500, xaxis=dict(visible=False), yaxis=dict(visible=False),
+                        title=f"Best Ensemble Size: {best_t}, Accuracy: {best_acc:.2f}"))
+    fig.write_html("q3_best_ensemble_surface.html", include_plotlyjs="cdn", full_html=True)
+
+
+def plot_test_train_err(test_err, train_err, ts):
     plt.figure(figsize=(8, 5))
     plt.plot(ts, train_err, label="Train Error")
     plt.plot(ts, test_err, label="Test Error")
@@ -140,6 +128,25 @@ def question_1(ab_model, n_learners, test_X, test_y, train_X, train_y):
     plt.tight_layout()
     plt.savefig("ada_train_test_error.png", dpi=300, bbox_inches="tight")
     plt.close()
+
+
+def comparing_ensemble_sizes(ab_model, test_X, test_y, lims):
+    T = [5, 50, 100, 250]
+    fig = make_subplots(rows=2, cols=2, subplot_titles=[f"T = {t}" for t in T], horizontal_spacing=0.05,
+                        vertical_spacing=0.07)
+    for i, t in enumerate(T):
+        r, c = divmod(i, 2)
+        row, col = r + 1, c + 1
+        fig.add_traces([
+            decision_surface(lambda X, T=t: ab_model.partial_predict(X, T), lims[0], lims[1], showscale=False),
+            go.Scatter(x=test_X[:, 0], y=test_X[:, 1], mode="markers", showlegend=False,
+                       marker=dict(color=test_y, symbol=[class_symbols[int((y_i + 1) // 2)] for y_i in test_y],
+                                   colorscale=px.colors.diverging.RdBu, line=dict(color="black", width=1), size=6))
+        ], rows=row, cols=col)
+        fig.update_xaxes(range=lims[0], row=row, col=col)
+        fig.update_yaxes(range=lims[1], row=row, col=col)
+    fig.update_layout(title_text="AdaBoost Partial Decision Surfaces (noise=0)", height=800, width=800, )
+    fig.write_html("q2_adaboost_surfaces.html", include_plotlyjs="cdn", full_html=True)
 
 
 if __name__ == '__main__':
